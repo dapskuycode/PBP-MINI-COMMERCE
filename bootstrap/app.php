2fcaebdc3b5249564ffe0fa342b00656966ba7a5
gi2br3n1906 <gi2br3n1906@gmail.com>
2025-10-13 13:54:08 +0000

    middleware no-admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'no-admin' => \App\Http\Middleware\RestrictAdminFromUserPages::class,
         ]);
         
         // Exclude CSRF protection for API testing
