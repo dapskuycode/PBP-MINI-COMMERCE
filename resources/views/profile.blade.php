d8b38abff6b9246f1c98d025c0f9c966aa9c67fb
Jihan <jihansaniyya2306@gmail.com>
2025-10-19 05:17:57 +0700

    menghilangkan Riwayat Pesan pada akun Saya

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -51,11 +51,6 @@
                               Profil Saya
                           </a>
                           @if(!$user->is_admin)
-                          <a href="{{ route('orders.index') }}"
-                             class="block rounded-lg px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                              Riwayat Pesanan
-                          </a>
-                          
                             <a href="{{ route('favorites') }}"
                                 class="block px-4 py-2 rounded text-sm
                                         {{ request()->routeIs('profile.favorites') ? 'bg-emerald-100 text-emerald-800 font-medium' : 'text-gray-700 hover:bg-gray-100' }}">
